e387afeeffcc9a737d4c05ef6b0e1d5ed56fc4c7
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2021-10-20 23:23:23 +0300

    Create Authorization_database.cs

---
+++ Authorization_database.cs
@@ -0,0 +1,14 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaumanBooking
+{
+    class Authorization_database
+    {
+        MySqlConnection connection = new MySqlConnection();
+    }
+}
